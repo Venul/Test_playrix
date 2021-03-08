147182df55f59e28277e28894c93534f56804968
Lunev Sergey <s.lunev88@gmail.com>
2021-03-09 01:53:39 +0300

    add stairs btn functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # playrix
+
+npm i
+for run - beefy index.js --live --watchify
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PIXI = require('pixi.js');
 
-function addSprite (img, posx, posy, width, height) {
+let addSprite =(img, posx, posy, width, height) => {
   const elem = PIXI.Sprite.from(img);
 
   elem.x = posx;
@@ -11,15 +11,61 @@ function addSprite (img, posx, posy, width, height) {
   app.stage.addChild(elem);
 }
 
-function onClickFinish() {
-  addSprite ('/img/final.png', 0, 0, appWidth, appHeight);
+  let onClickFinish = () => {
+    addSprite ('/img/final.png', 0, 0, appWidth, appHeight);
+  }
+
+  let onClickHammer = () => {
+  app.stage.removeChild(hammer);
+  app.stage.addChild(stair1);
+  gsap.to(stair1, {
+    alpha: 1, duration: 1
+  });
+  app.stage.addChild(stair2);
+  gsap.to(stair2, {
+    alpha: 1, duration: 1
+  });
+  app.stage.addChild(stair3);
+  gsap.to(stair3, {
+    alpha: 1, duration: 1
+  });
 }
 
-function onClickHammer() {
-  app.stage.removeChild(hammer);
-  addSprite ('/img/01_stair.png', appWidth * 0.63, 20);
-  addSprite ('/img/02_stair.png', appWidth * 0.7, 20);
-  addSprite ('/img/03_stair.png', appWidth * 0.77, 20);
+let onClickStair1 = () => {
+  stair1_active.visible = true;
+  stair2_active.visible = false;
+  stair3_active.visible = false;
+  stair1.visible = false;
+  stair2.visible = true;
+  stair3.visible = true;
+  btnOk.x = appWidth * 0.62;
+  btnOk.y = 130;
+  app.stage.addChild(btnOk);
+}
+
+let onClickStair2 = () => {
+  stair1_active.visible = false;
+  stair2_active.visible = true;
+  stair3_active.visible = false;
+  stair1.visible = true;
+  stair2.visible = false;
+  stair3.visible = true;
+
+  btnOk.x = appWidth * 0.69;
+  btnOk.y = 130;
+  app.stage.addChild(btnOk);
+}
+
+let onClickStair3 = () => {
+  stair1_active.visible = false;
+  stair2_active.visible = false;
+  stair3_active.visible = true;
+  stair1.visible = true;
+  stair2.visible = true;
+  stair3.visible = false;
+  btnOk.x = appWidth * 0.76;
+  btnOk.y = 130;
+  app.stage.addChild(btnOk);
 }
 
 const app = new PIXI.Application({
@@ -40,18 +86,18 @@ addSprite ('/img/austin.png', appWidth / 2, appHeight / 6,  appWidth / 12, appHe
 addSprite ('/img/stair.png', appWidth * 0.55, appHeight * 0.1, appWidth * 0.7, appHeight);
 addSprite ('/img/dec_1.png', appWidth * 0.8, appHeight * 0.68, appWidth * 0.2, appHeight * 0.4);
 
-const btn = PIXI.Sprite.from('/img/btn.png');
-btn.x = appWidth * 0.36;
-btn.y = appHeight * 0.73;
-btn.width = appWidth * 0.25;
-btn.height = appHeight * 0.2;
-btn.interactive = true;
-btn.buttonMode = true;
-btn.scale.x = btn.scale.y = 1;
-btn.on('pointerdown', onClickFinish);
+const btnContinue = PIXI.Sprite.from('/img/btn.png');
+btnContinue.x = appWidth * 0.36;
+btnContinue.y = appHeight * 0.73;
+btnContinue.width = appWidth * 0.25;
+btnContinue.height = appHeight * 0.2;
+btnContinue.interactive = true;
+btnContinue.buttonMode = true;
+btnContinue.scale.x = btnContinue.scale.y = 1;
+btnContinue.on('pointerdown', onClickFinish);
 
-gsap.to(btn.scale, 2, {duration: 0.5, x:1.1, y: 1.1,  ease: 'bounce.ease-in', repeat:-1, yoyo:true});
-app.stage.addChild(btn);
+gsap.to(btnContinue.scale, 2, {duration: 0.5, x:1.1, y: 1.1,  ease: 'bounce.ease-in', repeat:-1, yoyo:true});
+app.stage.addChild(btnContinue);
 
 const hammer = new PIXI.Sprite.from('/img/icon_hammer.png');
 hammer.y = appHeight * 0.47;
@@ -66,4 +112,52 @@ window.setTimeout(function() {
   gsap.to(hammer, {
     alpha: 1, duration: 1
   });
-}, 50);
+}, 3000);
+
+const stair1 = PIXI.Sprite.from('/img/01_stair.png');
+stair1.x = appWidth * 0.63;
+stair1.y = 20;
+stair1.interactive = true;
+stair1.buttonMode = true;
+stair1.alpha = 0;
+
+const stair2 = PIXI.Sprite.from('/img/02_stair.png');
+stair2.x = appWidth * 0.7;
+stair2.y = 20;
+stair2.interactive = true;
+stair2.buttonMode = true;
+stair2.alpha = 0;
+
+const stair3 = PIXI.Sprite.from('/img/03_stair.png');
+stair3.x = appWidth * 0.77;
+stair3.y = 20;
+stair3.interactive = true;
+stair3.buttonMode = true;
+stair3.alpha = 0;
+
+const stair1_active = PIXI.Sprite.from('/img/01_stair_active.png');
+stair1_active.x = appWidth * 0.63;
+stair1_active.y = 20;
+app.stage.addChild(stair1_active);
+stair1_active.visible = false;
+
+const stair2_active = PIXI.Sprite.from('/img/02_stair_active.png');
+stair2_active.x = appWidth * 0.7;
+stair2_active.y = 20;
+app.stage.addChild(stair2_active);
+stair2_active.visible = false;
+
+const stair3_active = PIXI.Sprite.from('/img/03_stair_active.png');
+stair3_active.x = appWidth * 0.77;
+stair3_active.y = 20;
+app.stage.addChild(stair3_active);
+stair3_active.visible = false;
+
+const btnOk = PIXI.Sprite.from('/img/ok.png');
+btnOk.interactive = true;
+btnOk.buttonMode = true;
+btnOk.on('pointerdown', onClickFinish);
+
+stair1.on('pointerdown', onClickStair1);
+stair2.on('pointerdown', onClickStair2);
+stair3.on('pointerdown', onClickStair3);
